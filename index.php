4fde067d5df4f10b5c68a90cb16f56f5a30edc40
unknown <deepak.sharma@devstringx.com>
2022-01-08 18:36:12 +0530

    build

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+#library 1
+
+// include ( 'PdfToText.php' );
+// $a = new PDFToText('test.pdf');	 
+// $data= $a->Text;
+// echo $data;
+
+
+#library 2
+
+// include('class.pdf2text.php');
+// $a = new PDF2Text();
+// $a->setFilename('test.pdf');
+// $a->decodePDF();
+// $data = $a->output(); 
+// echo $data;
